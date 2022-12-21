e13b42f9293f6d5dfc39909378a13fad9221c72a
Arno <97076594+arnoprincestonpan@users.noreply.github.com>
2022-12-20 20:26:37 -0800

    Added JavaScript Folder and Initial Assignment00

---
+++ JavaScript/Assignment 00 - COMP 2132/script/name.js
@@ -0,0 +1,6 @@
+//Arno's first Javascript!
+//Changes "Student Name" to "Arno Princeston Pan"
+function myName()
+{
+    document.getElementById("javascript_message").innerHTML = "Arno Princeston Pan";
+}
